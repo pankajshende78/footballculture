3188ad10c2e625abed944c6c492d2b948bb693fe
pankajshende78 <shendepankaj78@gmail.com>
2021-01-09 17:16:16 +0530

    First Commit

---
+++ signup.php
@@ -0,0 +1,22 @@
+<html>
+<head>
+<head>
+     <link rel="shortcut icon" href="Iron.ico" />
+     <link href="C:\Documents and Settings\Admin\Desktop\Photos\login\login.css" rel="stylesheet" type="text/css">
+     
+</head>
+</head>
+<body>
+<?php
+$dbconnect=mysqli_connect("localhost","root","football");
+if(mysqli_connect_errno($dbconnect))
+{
+echo "failed to connect";
+}
+else
+{
+echo "connection succesful";
+} 
+?>
+</body>
+</html>
